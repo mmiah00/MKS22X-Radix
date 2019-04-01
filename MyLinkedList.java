a5bd9c7b23d051e8a99223f94441c539a112a5b1
mmiah00 <mmiah00@stuty.edu>
2019-04-01 14:49:09 -0400

    need to test linkedlist

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -30,6 +30,7 @@ public class MyLinkedList<E> {
   public boolean add(E adding) {
     Node<E> a = new Node<E> (adding);
     end = a;
+    size ++; 
     return true;
   }
 
@@ -49,6 +50,15 @@ public class MyLinkedList<E> {
     start = start.next ();
     size --;
     return front;
+  }
+
+  public static void main (String[] args) {
+    MyLinkedList<Integer> test = new MyLinkedList <Integer> ();
+    for (int x = 0; x < 10; x ++) {
+      test.add (x);
+    }
+    System.out.println ("Size (should be 10): " + test.size ());
+    System.out.println ("List: "+ test.toString ());
   }
 
 }
