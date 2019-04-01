80218f61a476295a6b3c5c6f61d83e02501f4ecc
mmiah00 <mmiah00@stuty.edu>
2019-04-01 14:43:53 -0400

    found how many digits in the greatest number

--- Radix.java
+++ Radix.java
@@ -1,3 +1,22 @@
 public class Radix {
-  public static void radixsort(int[]data){}
+  public static void radixsort(int[]data){
+
+  }
+
+  private static int biggest (int[] data) { //look through list to find how many digits in the greatest number
+    int m = data[0];
+    for (int i = 1; i < data.length; i ++) {
+      if (Math.abs (data[i]) > Math.abs (m)) {
+        m = data[i];
+      }
+    }
+    return (int) (Math.log10 (Math.abs (m)) + 1);
+  }
+
+  public static void main (String[] args) {
+    System.out.println (biggest (new int [] {1,2,4,6,31,5,2}));
+    System.out.println (biggest (new int [] {100}));
+    System.out.println (biggest (new int [] {1000000,1,4,2}));
+    System.out.println (biggest (new int [] {-1111,2,4,23,100,41,1111}));
+  }
 }
