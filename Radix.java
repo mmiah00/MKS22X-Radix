9e4ae3c40d677b3094c81660c4f949f211a6ecfe
mmiah <mmiah00@stuy.edu>
2019-04-02 21:56:27 -0400

    working on the merging part

--- Radix.java
+++ Radix.java
@@ -5,21 +5,32 @@ public class Radix {
     int i = 0;
     int passes = biggest (data);
     MyLinkedList<Integer>[] buckets = new MyLinkedList [20];
+    MyLinkedList<Integer> ans = new MyLinkedList <> ();
     //MyLinkedList<Integer> temp = convert (data);
     while (i < passes) {
       int place = (int) Math.pow (10,i);
-      //digit is the number % 10 ^ i
+      //put numbers in buckets
       for (int x = 0; x < data.length; x ++) {
         if (data[x] >= 0) {
           int digit = data[x] / place % 10;
           buckets[digit + 10].add (data[x]);
         }
         else {
+          int digit = (Math.abs (data[x])) / place % 10;
+          buckets[9-digit].add (data[x]);
         }
       }
+
+      //merge to a linked list
+      for (int x = 0; x < buckets.length; x ++) {
+        ans.extend (buckets[x]);
+      }
+      
+
       i ++;
     }
   }
+
 
   @SuppressWarnings("unchecked")
   private static MyLinkedList <Integer> convert (int[] data) {
@@ -47,6 +58,6 @@ public class Radix {
     System.out.println (biggest (new int [] {1000000,1,4,2}));
     System.out.println (biggest (new int [] {-1111,2,4,23,100,41,1111}));
     */
-    System.out.println ("Hundreds Digit of 1239012 = " + (1239012/100%10));
+    System.out.println ("Ones Digit of 1239012 = " + (1239012/1%10));
   }
 }
